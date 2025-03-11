179010c5899316fac791904fab5d4bd64c1e15e4
Miroum <sartorisjeanpaul.mailpro@gmail.com>
2025-03-11 13:18:55 +0100

    Mise à jour de l'en-tête : modification du titre et suppression de la version du projet

--- SK/head.py
+++ SK/head.py
@@ -6,5 +6,4 @@ def head():
     with col1:
         st.image("http://sartoris.dscloud.mobi/img/logo.png", width=120)
     with col2:
-        st.header("OmniData Toolkit")
-        st.write("vertion de Streamlit : " + st.__version__ + ". vertion du preject : dev 1.3")
+        st.header("Swiss-knife")
